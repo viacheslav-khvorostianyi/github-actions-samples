c20fbd6623af6a0cf938ef54e1af7a9f6d7d9a44
aws user <aws.user12358@gmail.com>
2025-11-03 19:35:28 +0200

    Edit heelo.py

--- app/hello.py
+++ app/hello.py
@@ -7,7 +7,7 @@ config.read('config.properties')
 app = Flask(__name__)
 
 if config.getboolean("features", "feature_1") == True:
-	message = "Hello, Viacheslav!"
+	message = "Hello, Slava!"
 else:
 	message = "Hello, World!"
 
